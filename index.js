e1228c76dc5822df4f920fc24866ea3c08a2caa8
chandelierephrata <chandephrata@gmail.com>
2021-10-26 11:29:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employee= { 
+    Name: 'Paul',
+     Street:'Lincoln',
+    }
+    function updateEmployeeWithKeyAndValue(employee,key, value){
+        const newEmployee ={ ...object};
+        newEmployee[key] = value
+        return newEmployee;
+    }
